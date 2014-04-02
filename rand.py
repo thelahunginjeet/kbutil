34f24df21826d94846c36bcb5b462128db9d23a4
Kevin Brown <kevin.s.brown@uconn.edu>
2014-04-02 11:05:58 -0400

    Minor change to commments.

--- rand.py
+++ rand.py
@@ -1,13 +1,13 @@
 from numpy.random import randint
 
 def randbit(size=None):
-    '''
+    """
     Generates an array of shape size of random {0,1} bits.
-    '''
+    """
     return randint(2,size=size)
 
 def randspin(size=None):
-    '''
+    """
     Generates an array of shape size of random {-1,1} spin variables.
-    '''
+    """
     return 2*randbit(size=size) - 1
