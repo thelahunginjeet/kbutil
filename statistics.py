21b39c7683f9ba8e2f4065132b768a6f8d25f85a
Kevin Brown <kevin.s.brown@uconn.edu>
2016-04-08 13:39:20 -0400

    Added a discrete_frequency_calculator.

--- statistics.py
+++ statistics.py
@@ -6,31 +6,32 @@ This source code is provided under the BSD-3 license, duplicated as follows:
 Copyright (c) 2013, Kevin S. Brown
 All rights reserved.
 
-Redistribution and use in source and binary forms, with or without modification, 
+Redistribution and use in source and binary forms, with or without modification,
 are permitted provided that the following conditions are met:
 
-1. Redistributions of source code must retain the above copyright notice, this 
+1. Redistributions of source code must retain the above copyright notice, this
 list of conditions and the following disclaimer.
 
-2. Redistributions in binary form must reproduce the above copyright notice, this 
-list of conditions and the following disclaimer in the documentation and/or other 
+2. Redistributions in binary form must reproduce the above copyright notice, this
+list of conditions and the following disclaimer in the documentation and/or other
 materials provided with the distribution.
 
-3. Neither the name of the University of Connecticut  nor the names of its contributors 
-may be used to endorse or promote products derived from this software without specific 
+3. Neither the name of the University of Connecticut  nor the names of its contributors
+may be used to endorse or promote products derived from this software without specific
 prior written permission.
 
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS 
-OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY 
-AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR 
-CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL 
-DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, 
-DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER 
-IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT 
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS
+OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY
+AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
+IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
 OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 """
 
-from numpy import arange,correlate,newaxis,dot,sort,int,floor,ceil,interp,isnan,ones,asarray
+from numpy import arange,correlate,newaxis,dot,sort,int,floor,
+from numpy import ceil,interp,isnan,ones,asarray,argsort
 from numpy import hanning,hamming,bartlett,blackman,r_,convolve
 from numpy.random import randint
 from scipy.stats import pearsonr,spearmanr,kendalltau
@@ -40,7 +41,7 @@ def spearman_footrule_distance(s,t):
     Computes the Spearman footrule distance between two full lists of ranks:
 
         F(s,t) = (2/|S|^2)*sum[ |s(i) - t(i)| ],
-    
+
     the normalized sum over all elements in a set of the absolute difference between
     the rank according to s and t.  As defined, 0 <= F(s,t) <= 1.
 
@@ -50,7 +51,7 @@ def spearman_footrule_distance(s,t):
     # check that size of intersection = size of s,t?
     assert len(s) == len(t)
     return (2.0/len(s)**2)*sum(abs(asarray(s) - asarray(t)))
-    
+
 
 def standardize(X,stype='row'):
     """
@@ -58,7 +59,7 @@ def standardize(X,stype='row'):
     according to either rows or columns.
     """
     if stype == 'row':
-        return (X - X.mean(axis=1)[:,newaxis])/X.std(axis=1)[:,newaxis] 
+        return (X - X.mean(axis=1)[:,newaxis])/X.std(axis=1)[:,newaxis]
     return (X - X.mean(axis=0)[newaxis,:])/X.std(axis=0)[newaxis,:]
 
 
@@ -71,7 +72,7 @@ def autocovariance(X,norm=False):
 
     This version computes phi(T) at all T (0,...N-1).
 
-    If norm = True, the autocorrelation (phi(T)/phi(0)), rather than the 
+    If norm = True, the autocorrelation (phi(T)/phi(0)), rather than the
     bare autocovariance is returned.
     """
     Xp = X - X.mean()
@@ -97,7 +98,7 @@ def corrmatrix(X,Y):
 def empirical_ci(x,alpha=0.05):
     """
     Computes an empirial (alpha/2, 1-alpha/2) confidence interval for the distributional data
-    in x.  Returns a tuple (lb,ub) which are the lower and upper bounds for the coverage 
+    in x.  Returns a tuple (lb,ub) which are the lower and upper bounds for the coverage
     interval.
     """
     assert alpha > 0.0
@@ -115,13 +116,13 @@ def empirical_ci(x,alpha=0.05):
 
 def bootstrap_correlation(x,y,cType='pearson',p=0.05,N=5000):
     """
-    Computes a simple bootstrap CI on simple correlation coefficients.  The CI is 
+    Computes a simple bootstrap CI on simple correlation coefficients.  The CI is
     computed using the interval method.  This won't work properly if x or y are time-series
-    with significant autocorrelation.  You need to downsample or use a more complicated 
-    bootstrap that preserves that structure.  
-    
+    with significant autocorrelation.  You need to downsample or use a more complicated
+    bootstrap that preserves that structure.
+
     Parameters:
-    ------------    
+    ------------
     x,y   : (1D) lists or arrays of data
     cType : string, optional
             should be 'pearson', 'spearman', or 'kendall'
@@ -159,28 +160,28 @@ def bootstrap_correlation(x,y,cType='pearson',p=0.05,N=5000):
 
 def smooth(x,wlen=11,window='flat'):
     """Smooth the data using a window with requested size.
-        
+
     This method is based on the convolution of a scaled window with the signal.
-    The signal is prepared by introducing reflected copies of the signal 
+    The signal is prepared by introducing reflected copies of the signal
     (with the window size) in both ends so that transient parts are minimized
     in the begining and end part of the output signal.
-       
+
     Parameters:
     ------------
      x      : array type, required
               the input signal to smoothed
-     wlen   : integer, odd, optional 
+     wlen   : integer, odd, optional
               the size of the smoothing window; should be an odd integer
-     window : string, optional 
+     window : string, optional
               the type of window. allowed choices are:
                  'flat', 'hanning', 'hamming', 'bartlett', 'blackman'
               'flat' produces a moving average.
 
     Returns:
     ------------
-    y : array type 
+    y : array type
         the smoothed signal
-                 
+
     Example:
     ------------
     t=linspace(-2,2,0.1)
@@ -211,5 +212,33 @@ def smooth(x,wlen=11,window='flat'):
     return y[wlen:-wlen+1]
 
 
+def discrete_frequency_calculator(intList):
+    '''
+    Accepts a list of integer values, returning two numpy arrays of values and frequencies.
+    The output frequencies are normalized so that sum(Pn) = 1.0.  Input values are requried
+    to be integer; no binning is performed for floating point values.
 
+    INPUT
+    ------
+    intList: list of integers, required
+             input list of data of integer type
 
+    OUTPUT
+    ------
+    n,Pn : two lists of values and freq(values)
+           frequencies are normalized so that sum(Pn) = 1.0, and the two returned
+           arrays are in 1-1 correspondence, sorted in order of increasing n
+
+    '''
+    assert all([type(x) == int for x in intList])
+    freq = {}
+    n = len(intList)
+    for k in intList:
+        pkinc = 1.0/n
+        if not freq.has_key(k):
+            freq[k] = pkinc
+        else:
+            freq[k] += pkinc
+    # sorting
+    indx = argsort(freq.keys())
+    return asarray([freq.keys()[x] for x in indx]),asarray([freq.values()[x] for x in indx])
